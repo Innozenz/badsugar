7974b936cc7d33aa3abf41b68d6ee3728b4b2c51
Maxime <mfitzjean@gmail.com>
2023-06-24 16:17:42 +0200

    API problems

--- next.config.js
+++ next.config.js
@@ -7,14 +7,6 @@ const nextConfig = {
   api: {
     bodyParser: false
   },
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'https://badsugar.vercel.app/:path*',
-      },
-    ]
-  },
 }
 
 module.exports = nextConfig
