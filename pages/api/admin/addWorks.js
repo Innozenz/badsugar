cf07fbffb7a9d3a20d7e5fc9130e3e11d296e8b1
Maxime <mfitzjean@gmail.com>
2023-06-24 15:46:15 +0200

    API problems

--- pages/api/admin/addWorks.js
+++ pages/api/admin/addWorks.js
@@ -37,6 +37,7 @@ export default async function handler(req, res) {
             await work.save();
 
             res.status(200).json({ message: 'Travail ajouté avec succès.' });
+            res.redirect(303, '/')
         } catch (error) {
             console.error(error);
             res.status(500).json({ message: 'Une erreur s\'est produite lors de l\'ajout du travail.' });
